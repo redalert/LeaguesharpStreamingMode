fa4c7269cb5bc19571d4437727e9e8188a712864
redalert <masktr1@hotmail.com>
2015-01-02 15:20:59 +0200

    Update Program.cs

--- LeaguesharpStreamingMode/Program.cs
+++ LeaguesharpStreamingMode/Program.cs
@@ -16,12 +16,7 @@ namespace LeaguesharpStreamingMode
         {
             SetUpOffsets();
             Enable();
-
             LeagueSharp.Game.OnWndProc += OnWndProc;
-            AppDomain.CurrentDomain.DomainUnload += delegate
-            {
-                Disable();
-            };
         }
 
         static Int32 GetModuleAddress(String ModuleName)
@@ -60,7 +55,8 @@ namespace LeaguesharpStreamingMode
             drawEvent = 0,
             printChat = 1,
             loadingScreenWatermark = 2,
-            watermarkChanging = 3
+            watermarkChanging = 3,
+            changePrintChat = 4
         }
 
         enum asm : byte
@@ -75,7 +71,7 @@ namespace LeaguesharpStreamingMode
             offsets = new Dictionary<string, Int32[]>();
             offsets.Add("4.19", new Int32[] { 0x5F40, 0x9B60, 0x9B40 });
             offsets.Add("4.20", new Int32[] { 0x6040, 0x9C00, 0x9BE0 });
-            offsets.Add("4.21", new Int32[] { 0x6420, 0xA230, 0xA1B5, 0xDA30 });
+            offsets.Add("4.21", new Int32[] { 0x6420, 0xA230, 0xA1B5, 0xDA30, 0xD8C8 });
         }
 
         static void Enable()
@@ -87,15 +83,49 @@ namespace LeaguesharpStreamingMode
             WriteMemory(LeaguesharpCore + offsets[version][(int)functionOffset.watermarkChanging],      new byte[] { (byte)0x57, (byte)0x48, (byte)0x4B, 
                                                                                                          (byte)0x44, (byte)0x4C, (byte)0x6F, (byte)0x61, 
                                                                                                          (byte)0x64, (byte)0x65, (byte)0x72, (byte)0x20 });
+        
+            WriteMemory(LeaguesharpCore + offsets[version][(int)functionOffset.changePrintChat]       new byte[] { (byte)0x3C,(byte)0x66 ,(byte)0x6F ,
+                                                                                                         (byte)0x6E,(byte)0x74,(byte)0x20,(byte)0x63,
+                                                                                                         (byte)0x6F,(byte)0x6C,(byte)0x6F,(byte)0x72,
+                                                                                                         (byte)0x3D,(byte)0x22,(byte)0x23,(byte)0x34,
+                                                                                                         (byte)0x30,(byte)0x63,(byte)0x31,(byte)0x66,
+                                                                                                         (byte)0x66,(byte)0x22,(byte)0x3E,(byte)0x57,
+                                                                                                         (byte)0x48,(byte)0x4B,(byte)0x44,(byte)0x4C,
+                                                                                                         (byte)0x6F,(byte)0x61,(byte)0x64,(byte)0x65,
+                                                                                                         (byte)0x72,(byte)0x20 ,(byte)0x2D ,(byte)0x2D ,
+                                                                                                         (byte)0x20 ,(byte)0x76 ,(byte)0x65 ,(byte)0x72 ,
+                                                                                                         (byte)0x73 ,(byte)0x2E ,(byte)0x3C ,(byte)0x2F ,
+                                                                                                         (byte)0x66 ,(byte)0x6F ,(byte)0x6E ,(byte)0x74 ,
+                                                                                                         (byte)0x3E ,(byte)0x20 ,(byte)0x3C ,(byte)0x66 ,
+                                                                                                         (byte)0x6F ,(byte)0x6E ,(byte)0x74 ,(byte)0x20 ,
+                                                                                                         (byte)0x63 ,(byte)0x6F ,(byte)0x6C ,(byte)0x6F ,
+                                                                                                         (byte)0x72 ,(byte)0x3D ,(byte)0x22 ,(byte)0x23 ,
+                                                                                                         (byte)0x33 ,(byte)0x39 ,(byte)0x46 ,(byte)0x46 ,
+                                                                                                         (byte)0x31 ,(byte)0x34 ,(byte)0x22 ,(byte)0x3E ,
+                                                                                                         (byte)0x25 ,(byte)0x73 ,(byte)0x20 ,(byte)0x25 ,
+                                                                                                         (byte)0x73 ,(byte)0x3C ,(byte)0x2F ,(byte)0x66 ,
+                                                                                                         (byte)0x6F ,(byte)0x6E ,(byte)0x74 ,(byte)0x3E ,
+                                                                                                         (byte)0x00 ,(byte)0x00 ,(byte)0x00 ,(byte)0x00 ,
+                                                                                                         (byte)0x00 ,(byte)0x62 ,(byte)0x79 ,(byte)0x20 ,
+                                                                                                         (byte)0x57 ,(byte)0x48 ,(byte)0x4B ,(byte)0x44 ,
+                                                                                                         (byte)0x20 ,(byte)0x2D ,(byte)0x20 ,(byte)0x73 ,
+                                                                                                         (byte)0x6B ,(byte)0x79 ,(byte)0x70 ,(byte)0x65 ,
+                                                                                                         (byte)0x3A ,(byte)0x72 ,(byte)0x6F ,(byte)0x73 ,
+                                                                                                         (byte)0x65 ,(byte)0x6E ,(byte)0x62 ,(byte)0x65 ,
+                                                                                                         (byte)0x72 ,(byte)0x67 ,(byte)0x63 ,(byte)0x69 ,
+                                                                                                         (byte)0x6B ,(byte)0x20 ,(byte)0x2D ,(byte)0x20 ,
+                                                                                                         (byte)0x45 ,(byte)0x6C ,(byte)0x6D ,(byte)0x6F ,
+                                                                                                         (byte)0x20 ,(byte)0x4B ,(byte)0x65 ,(byte)0x6E ,
+                                                                                                         (byte)0x6E ,(byte)0x65 ,(byte)0x64 ,(byte)0x79 ,
+                                                                                                         (byte)0x20 ,(byte)0x6F ,(byte)0x6E ,(byte)0x20 ,
+                                                                                                         (byte)0x46 ,(byte)0x61 ,(byte)0x63 ,(byte)0x65 ,
+                                                                                                         (byte)0x62 ,(byte)0x6F ,(byte)0x6F ,(byte)0x6B ,
+                                                                                                         (byte)0x20 ,(byte)0x2D ,(byte)0x20 ,(byte)0x73 ,
+                                                                                                         (byte)0x61 ,(byte)0x64 ,(byte)0x62 ,(byte)0x6F ,
+                                                                                                         (byte)0x79 ,(byte)0x73 ,(byte)0x20 ,(byte)0x32 ,
+                                                                                                         (byte)0x30 ,(byte)0x30 ,(byte)0x35 });
+            
         }
-
-        static void Disable()
-        {
-            WriteMemory(LeaguesharpCore + offsets[version][(int)functionOffset.drawEvent], (byte)asm.push_ebp);
-            WriteMemory(LeaguesharpCore + offsets[version][(int)functionOffset.printChat], (byte)asm.push_ebp);
-        }
-
-        static bool IsEnabled() { return ReadMemory(LeaguesharpCore + offsets[version][(int)functionOffset.printChat], 1)[0] == (byte)asm.ret; }
 
         static void OnWndProc(LeagueSharp.WndEventArgs args)
         {
